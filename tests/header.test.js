02b60652f0297d02ee11d0c9e8b3daeef9c48e8b
erocignolo <a.edisonazevedo@gmail.com>
2020-07-11 12:36:07 -0300

    comentado chamada dos testes no Travis

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,60 +1,34 @@
-const puppeteer = require('puppeteer')
+const Page = require('./helpers/page');
 
-let browser, page
+let page;
 
 beforeEach(async () => {
-    browser = await puppeteer.launch({
-        // headless: false === com interface grafica
-        headless: true,
-        // --no-sandbox diminui o tempo de execucao dos testes
-        args: ['--no-sandbox']
-    })
-    // criando uma tab no novagador
-    page = await browser.newPage()
-    await page.goto('http://localhost:3000')
-})
+  page = await Page.build();
+  await page.goto('http://localhost:3000');
+});
 
 afterEach(async () => {
-    await browser.close()
-})
+  await page.close();
+});
 
-test('Validando o texto do header', async () => {
-    // pegar o texto do link principal do blog
-    // esse trecho (el => el.innerHTML) é transmitido para o navegador como texto
-    // o navegador interpreta e retorna como texto tb
-    const text = await page.$eval('a.brand-logo', el => el.innerHTML)
-    expect(text).toEqual('Blogster')
-})
+test('the header has the correct text', async () => {
+  const text = await page.getContentsOf('a.brand-logo');
 
-test('Clicando no login e iniciando o fluxo oauth', async () => {
-    await page.click('.right a')
-    // verificando se a pagina esta no dominio do google
-    const url = await page.url()
-    expect(url).toMatch(/accounts\.google\.com/)
-})
+  expect(text).toEqual('Blogster');
+});
 
-test.only('Logando e mostrando botao de loggout', async () => {
-    // _id do mongo
-    const id = '5f05009a1e3fe3136096b468'
-    const Buffer = require('safe-buffer').Buffer
-    const sessionObject = {
-        passport: {
-            user: id
-        }
-    }
-    const sessionString = Buffer.from(JSON.stringify(sessionObject)).toString('base64')
-    const Keygrip = require('keygrip')
-    const keys = require('../config/keys')
-    const keygrip = new Keygrip([keys.cookieKey])
-    const sig = keygrip.sign('session=' + sessionString)
+test('clicking login starts oauth flow', async () => {
+  await page.click('.right a');
 
-    await page.setCookie({ name: 'session', value: sessionString })
-    await page.setCookie({ name: 'session.sig', value: sig })
-    // refresh the page to simulate the login
-    await page.goto('http://localhost:3000')
+  const url = await page.url();
 
-    await page.waitFor('a[href="/auth/logout"]')
-    const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML)
-    expect(text).toEqual('Logout')
-})
+  expect(url).toMatch(/accounts\.google\.com/);
+});
 
+test('When signed in, shows logout button', async () => {
+  await page.login();
+
+  const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML);
+
+  expect(text).toEqual('Logout');
+});
